d2c8ebddaf1ad074a6f2eb61bfbec0779f78ed85
Eric Farber <eric.j.farber@gmail.com>
2017-03-29 22:44:53 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+
+  languages.each do |key, kind|
+    #   binding.pry
+
+      kind.each do |attribute, data|
+          if !new_hash[attribute]
+              style = []
+          else
+              style = [].push(:oo)
+          end
+
+        #  binding.pry
+          new_hash[attribute] = {type: data.values.join, style: style.push(key) }
+        #   binding.pry
+
+    end
+    #   new_hash[name.keys()] = { type: name.values, kind: languages.values }
+    #    binding.pry
+  end
+
+  new_hash
+
 end
